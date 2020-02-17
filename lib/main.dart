51c4b97cf106faff60906aa6b2cf6d30020fef2a
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-17 20:09:12 +0300

    Added basic implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +9,63 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Note(
+                  noteKey: 1,
+                  color: Colors.red,
+                ),
+                Note(
+                  noteKey: 2,
+                  color: Colors.orange,
+                ),
+                Note(
+                  noteKey: 3,
+                  color: Colors.yellow,
+                ),
+                Note(
+                  noteKey: 4,
+                  color: Colors.green,
+                ),
+                Note(
+                  noteKey: 5,
+                  color: Colors.blue,
+                ),
+                Note(
+                  noteKey: 6,
+                  color: Colors.lightBlue,
+                ),
+                Note(
+                  noteKey: 7,
+                  color: Colors.purple,
+                )
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class Note extends StatelessWidget {
+  final noteKey;
+  final color;
+  Note({@required this.noteKey, @required this.color}) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: this.color,
+      onPressed: () {
+        final player = AssetsAudioPlayer();
+        player.open(
+          'assets/note${this.noteKey}.wav',
+        );
+        player.play();
+      },
+      child: null,
     );
   }
 }
